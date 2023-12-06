a2499e64eaf293fc22df2d0e771e158da63c7272
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-12-06 22:37:10 +0530

    user schema created and db connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './Models/user.model';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/urlShortener'),
+            MongooseModule.forFeature([{name:'user',schema:UserSchema}])],
   controllers: [AppController],
   providers: [AppService],
 })
