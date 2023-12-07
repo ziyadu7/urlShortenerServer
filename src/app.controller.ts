1d633792c0f8dd8f52031602d08a31dd8b150409
Muhammed Ziyad <muhdziyadu@gmail.com>
2023-12-07 09:16:08 +0530

    validation set to utils

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,20 @@
 import { Body, Controller, Get, HttpStatus, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AppService } from './app.service';
 import { userValidationDto } from './dto/user.dto';
+import { SETTINGS } from './app.utils';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('/register')
-  @UsePipes(ValidationPipe)
+  @UsePipes(SETTINGS.userValidation)
   async register(@Body() userDto:userValidationDto){
     return this.appService.Register(userDto)
   }
 
   @Post('/login')
-  @UsePipes(new ValidationPipe({errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY}))
+  @UsePipes(SETTINGS.userValidation)
   async login(@Body() userDto:userValidationDto){
     return this.appService.Login(userDto)
   }
--- src/app.utils.ts
+++ src/app.utils.ts
@@ -1,5 +1,12 @@
+import {ValidationPipe,HttpStatus} from '@nestjs/common'
+
 const usernameErrorMessage = 'Username is required'
 const passwrodErrorMessage = 'Password is required'
+const userValidation = new ValidationPipe({errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY})
+
+export const SETTINGS = {
+    userValidation
+}
 
 export const MESSAGES = {
     usernameErrorMessage,
